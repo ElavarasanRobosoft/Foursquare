6550ec42b65cb809d59acd56b9d257179df51337
ElavarasanRobosoft <elavarasan.t@robosoftin.com>
2022-12-14 14:37:33 +0530

    Activity change

--- app/src/main/java/com/robosoft/foursquare/view/activity/LoginActivity.kt
+++ app/src/main/java/com/robosoft/foursquare/view/activity/LoginActivity.kt
@@ -3,16 +3,16 @@ package com.robosoft.foursquare.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.robosoft.foursquare.R
-import com.robosoft.foursquare.databinding.ActivityMainBinding
+import com.robosoft.foursquare.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
